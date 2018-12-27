4f51c15aefce2d56abbfdf5b7591ad27a33368b9
tanleivtradex <15221228754@163.com>
2018-12-27 15:58:10 +0800

    IDEA整合Git

---
+++ boy/src/main/java/com/tanlei/boy/BoyApplication.java
@@ -0,0 +1,15 @@
+package com.tanlei.boy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoyApplication.class, args);
+
+	}
+
+}
+
